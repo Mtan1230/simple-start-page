8d13f6d5ccbdeae4fd3d4b038a9eb43d1550e1d5
Mtan1230 <m.tan1230@gmail.com>
2023-05-09 23:37:21 -0400

    complete feature/date-time

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,3 +1,12 @@
-const user = {
+let user = {
     name: '',
 };
+
+function loadStorage() {
+    const savedUser = JSON.parse(localStorage.getItem('user'));
+    if (savedUser) {
+        user = savedUser;
+    }
+}
+
+loadStorage();
--- assets/js/date_time.js
+++ assets/js/date_time.js
@@ -1,31 +1,48 @@
   const curtTime = $('#curtTime');
   const curtDate = $('#curtDate');
   const welcMsg = $('#welcMsg');
-  let userName = user.name;
+  const nameInput = $('#name');
 
   function setDateTime() {
-    curtTime.text(dayjs().format('h:mm a'));
+    curtTime.text(dayjs().format('h:mm'));
     curtDate.text(dayjs().format('MMM D, dddd'));
-    welcMsg.text('Hello ' + userName);
+    if (!user.name) {
+      nameInput.css('display', 'inline-block')
+    }
+    welcMsg.text('Hello, ' + user.name);
     setInterval(function () {
-      curtTime.text(dayjs().format('h:mm a'));
-
-      let hour = dayjs().hour();
-      switch (true) {
-        case (hour < 12):
-          welcMsg.text('Good morning! ' + user.name.uppercase());
-          break;
-        case (hour < 18):
-          welcMsg.text('Good afternoon! ' + user.name.uppercase());
-          break;
-        default:
-          welcMsg.text('Good evening! ' + user.name.uppercase());
-          break;
+      curtTime.text(dayjs().format('h:mm'));
+      const hour = dayjs().hour();
+      if (hour === 0) {
+        curtDate.text(dayjs().format('MMM D, dddd'));
       }
-
-      // if (dayjs().hour() === 0) {
-      //   curtDate.text(dayjs().format('MMM D, dddd'));
-      // }
+      if (user.name) {
+        setTimeout(function () {
+          switch (true) {
+            case (hour < 12):
+              welcMsg.text('Good morning, ' + user.name);
+              break;
+            case (hour < 18):
+              welcMsg.text('Good afternoon, ' + user.name);
+              break;
+            default:
+              welcMsg.text('Good evening, ' + user.name);
+              break;
+          }
+        }, 2000);
+      }
     }, 1000);
   }
+
   setDateTime();
+
+  //listen to the name input and save on local storage
+  nameInput.on('keydown', function(e) {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      user.name = nameInput.val();
+      nameInput.css('display', 'none')
+      welcMsg.text('Hello, ' + user.name);
+      localStorage.setItem("user", JSON.stringify(user));
+    }
+  })
