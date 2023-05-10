d5e5c1fa50bb09c0d5634f660ca958bd112da7ce
Mtan1230 <m.tan1230@gmail.com>
2023-05-09 20:18:55 -0400

    add variable to date_time.js

--- assets/js/date_time.js
+++ assets/js/date_time.js
@@ -1,16 +1,31 @@
   const curtTime = $('#curtTime');
   const curtDate = $('#curtDate');
   const welcMsg = $('#welcMsg');
+  let userName = user.name;
 
   function setDateTime() {
     curtTime.text(dayjs().format('h:mm a'));
     curtDate.text(dayjs().format('MMM D, dddd'));
-    welcMsg.text('Hello ' + user.name);
+    welcMsg.text('Hello ' + userName);
     setInterval(function () {
       curtTime.text(dayjs().format('h:mm a'));
-      if (dayjs().hour() === 0) {
-        curtDate.text(dayjs().format('MMM D, dddd'));
+
+      let hour = dayjs().hour();
+      switch (true) {
+        case (hour < 12):
+          welcMsg.text('Good morning! ' + user.name.uppercase());
+          break;
+        case (hour < 18):
+          welcMsg.text('Good afternoon! ' + user.name.uppercase());
+          break;
+        default:
+          welcMsg.text('Good evening! ' + user.name.uppercase());
+          break;
       }
+
+      // if (dayjs().hour() === 0) {
+      //   curtDate.text(dayjs().format('MMM D, dddd'));
+      // }
     }, 1000);
   }
   setDateTime();
