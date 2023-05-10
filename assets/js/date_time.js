134ad3a741a1c3b06caa9f40ee59a96559460265
Mtan1230 <m.tan1230@gmail.com>
2023-05-09 23:59:15 -0400

    capitalized input name and remove the background-color of the input

--- assets/js/date_time.js
+++ assets/js/date_time.js
@@ -1,48 +1,50 @@
-  const curtTime = $('#curtTime');
-  const curtDate = $('#curtDate');
-  const welcMsg = $('#welcMsg');
-  const nameInput = $('#name');
+const curtTime = $('#curtTime');
+const curtDate = $('#curtDate');
+const welcMsg = $('#welcMsg');
+const nameInput = $('#name');
 
-  function setDateTime() {
-    curtTime.text(dayjs().format('h:mm'));
-    curtDate.text(dayjs().format('MMM D, dddd'));
-    if (!user.name) {
-      nameInput.css('display', 'inline-block')
-    }
-    welcMsg.text('Hello, ' + user.name);
-    setInterval(function () {
-      curtTime.text(dayjs().format('h:mm'));
-      const hour = dayjs().hour();
-      if (hour === 0) {
-        curtDate.text(dayjs().format('MMM D, dddd'));
-      }
-      if (user.name) {
-        setTimeout(function () {
-          switch (true) {
-            case (hour < 12):
-              welcMsg.text('Good morning, ' + user.name);
-              break;
-            case (hour < 18):
-              welcMsg.text('Good afternoon, ' + user.name);
-              break;
-            default:
-              welcMsg.text('Good evening, ' + user.name);
-              break;
-          }
-        }, 2000);
-      }
-    }, 1000);
+function setDateTime() {
+  curtTime.text(dayjs().format('h:mm'));
+  curtDate.text(dayjs().format('MMM D, dddd'));
+  if (!user.name) {
+    nameInput.css('display', 'inline-block')
   }
-
-  setDateTime();
-
-  //listen to the name input and save on local storage
-  nameInput.on('keydown', function(e) {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      user.name = nameInput.val();
-      nameInput.css('display', 'none')
-      welcMsg.text('Hello, ' + user.name);
-      localStorage.setItem("user", JSON.stringify(user));
+  welcMsg.text('Hello, ' + user.name);
+  setInterval(function () {
+    curtTime.text(dayjs().format('h:mm'));
+    const hour = dayjs().hour();
+    if (hour === 0) {
+      curtDate.text(dayjs().format('MMM D, dddd'));
     }
-  })
+    if (user.name) {
+      setTimeout(function () {
+        switch (true) {
+          case (hour < 12):
+            welcMsg.text('Good morning, ' + user.name);
+            break;
+          case (hour < 18):
+            welcMsg.text('Good afternoon, ' + user.name);
+            break;
+          default:
+            welcMsg.text('Good evening, ' + user.name);
+            break;
+        }
+      }, 2000);
+    }
+  }, 1000);
+}
+
+setDateTime();
+
+//listen to the name input and save on local storage
+nameInput.on('keydown', function (e) {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    let inputName = nameInput.val();
+    let capitalized = inputName.charAt(0).toUpperCase() + inputName.slice(1);
+    user.name = capitalized;
+    nameInput.css('display', 'none')
+    welcMsg.text('Hello, ' + user.name);
+    localStorage.setItem("user", JSON.stringify(user));
+  }
+})
