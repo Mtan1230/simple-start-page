7b70393ef5c730fed7c6cfc896e8487fd4961122
Your Name <you@example.com>
2023-05-11 21:39:46 -0400

    changed html to open search results in google window

--- assets/js/search-engine.js
+++ assets/js/search-engine.js
@@ -1,7 +1,7 @@
 function myFunction(){
     // Store search keyword
     var x = document.getElementById("search");
-    const url = "https://www.google.com/";
+    const url = "https://www.google.com/?#q=text_to_search";
     window.open(url);
     var url1 = url + x;
     var win = window.open(url1, '_blank');
