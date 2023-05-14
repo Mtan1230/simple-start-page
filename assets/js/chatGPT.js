eb562c19ffc2acd31b0179b9ac4343cbce26b532
Mtan1230 <m.tan1230@gmail.com>
2023-05-14 14:09:41 -0400

    add event listener to input message and adjust styling

--- assets/js/chatGPT.js
+++ assets/js/chatGPT.js
@@ -1,9 +1,30 @@
-async function chat() {
+const inputQ = document.getElementById('input-question');
+const sentQ = document.getElementById('sent-question');
+const chatContainer = document.getElementById('chat-container');
+
+function displayInput() {
+    const question = inputQ.value;
+    const displayQ = document.createElement('p');
+    displayQ.textContent = question;
+    displayQ.setAttribute('class', 'float-right');
+    chatContainer.appendChild(displayQ);
+    inputQ.value = '';
+}
+
+sentQ.addEventListener('click', displayInput);
+
+inputQ.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        displayInput();
+    }
+});
+
+async function sentChat() {
     url = `https://api.openai.com/v1/chat/completions`;
     await fetch(url, {
         method: "POST",
         headers: {
-            Authorization: `Bearer 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`,
+            Authorization: `Bearer ${user.openAI.key}`,
             "content-Type": "application/json"
         },
         body: JSON.stringify({
@@ -24,6 +45,3 @@ async function chat() {
             console.error(err);
         })
 }
-
-console.log(`${user.openAI.key}`)
-chat();
