4fca65dfb6b18b27b9865ed64d0b7ed6bb3f21ee
Mtan1230 <m.tan1230@gmail.com>
2023-05-15 20:24:31 -0400

    add feature to generate quotes and display at the bottom

---
+++ assets/js/quote.js
@@ -0,0 +1,28 @@
+const quote = document.getElementById('quote');
+const author = document.getElementById('author');
+
+async function getQuotes() {
+    const key = 'LF0jwI+EzK3wwore/KkY8A==uiXxwWPhQDdHhurV';
+    const url = 'https://api.api-ninjas.com/v1/quotes?category=happiness';
+
+    await fetch(url, {
+        headers: { 'X-Api-Key': key },
+        contentType: 'application/json',
+    })
+        .then(res => {
+            if (res.status === 404) {
+                console.log('404');
+            }
+            return res.json();
+        })
+        .then(data => {
+            console.log(data);
+            quote.textContent = `"` + data[0].quote + `"`;
+            author.textContent = data[0].author;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+getQuotes();
