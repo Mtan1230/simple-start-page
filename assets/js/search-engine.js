2a4c6673ddc0578626406b118a6fad1cc9a35d51
Mtan1230 <m.tan1230@gmail.com>
2023-05-16 21:13:54 -0400

    add styling to search bar

--- assets/js/search-engine.js
+++
@@ -1,9 +0,0 @@
-function myFunction(){
-    // Store search keyword
-    var x = document.getElementById("search");
-    const url = "https://www.google.com/?#q=text_to_search";
-    window.open(url);
-    var url1 = url + x;
-    var win = window.open(url1, '_blank');
-    win.focus;
-}
